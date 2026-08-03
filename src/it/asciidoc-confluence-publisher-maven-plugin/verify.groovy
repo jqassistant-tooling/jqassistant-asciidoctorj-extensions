f00657b4b6916cf026ccc669b4925b33a09a9e17
Johanna Lederer <johanna.lederer@buschmais.com>
2026-08-03 15:22:51 +0200

    #30 maven-confluence publisher fixed

--- src/it/asciidoc-confluence-publisher-maven-plugin/verify.groovy
+++ src/it/asciidoc-confluence-publisher-maven-plugin/verify.groovy
@@ -1,1 +1,3 @@
-
+def indexHtmlFile = new File(basedir, 'target/asciidoc-confluence-publisher/assets/975367e2001eda39fe8c811ff454339ca90f60c00d3882ea586142233d3020fc/index.html')
+assert indexHtmlFile.exists()
+assert indexHtmlFile.text.contains("it:CSVReport")
